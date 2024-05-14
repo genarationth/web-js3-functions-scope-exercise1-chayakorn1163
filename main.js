834b7b3c4ebe3bd6f18d38d6131fa7b765f13ca4
chayakon11643 <chayakon.prathumchai@gmail.com>
2024-05-14 15:31:53 +0700

    first-commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Exercise 1.1 Create function following below ://
+
+
+function add (a,b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+const result = add (10,55);
+console.log(result);
+
+
+//Exercise 1.2 Create Function Inform---//
+function inform(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ". I live in " + location + " and enjoy " + hobby + ".");
+}
+
+
+inform("John", "New York", "reading");
+
+
+
+
+
